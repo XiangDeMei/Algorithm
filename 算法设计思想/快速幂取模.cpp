7639cef30ca9145347e322fc4e512df74cf78fcc
CLAY 、 <CLAY >
2016-09-09 20:08:57 +0800

    9/9

--- 算法设计思想/快速幂取模.cpp
+++ 算法设计思想/快速幂取模.cpp
@@ -1,12 +1,26 @@
 //快速幂取模B^LmodP(O(logb))
+// a^b % c = (a^2)^(b/2) % c  b为偶数
+// a^b % c = (a^2)^(b/2)*a % c  b为奇数
 typedef long long ll;
-int power(int B, int L, int P)
-{
-	int acc = 1, q;
-	for(q = L; q; q >>= 1)
-	{
-		if(q & 1)	acc = (ll)acc * B % P;
-		B = (ll)B * B % P;
-	}
-	return acc;
+int power(int B, int L, int P) {
+  int acc = 1, q;
+  for (q = L; q; q >>= 1) {
+    if (q & 1)
+      acc = (ll)acc * B % P;
+    B = (ll)B * B % P;
+  }
+  return acc;
+}
+
+typedef long long ll;
+int power(ll a, ll b, int mod) {
+  ll res = 1;
+  ll base = a % mod;
+  while (b) {
+    if (b & 1)
+      res = (res * base) % mod;
+    base = (base * base) % mod;
+    b >>= 1;
+  }
+  return res;
 }
--- 算法设计思想/等比数列快速幂.cpp
+++ 算法设计思想/等比数列快速幂.cpp
@@ -1,23 +1,26 @@
 #define MOD 1000000007
 typedef long long LL;
 
-LL a_b_MOD_c(LL a,LL b,LL mod){
-	LL ret = 1;
-	a %= mod;
-	while(b){
-		if(b & 1) ret = ret * a % mod;
-		a = a * a % mod;
-		b >>= 1;
-	}
-	return ret;
+LL powmod(LL a, LL b, LL mod) {
+  LL ret = 1;
+  a %= mod;
+  while (b) {
+    if (b & 1)
+      ret = ret * a % mod;
+    a = a * a % mod;
+    b >>= 1;
+  }
+  return ret;
 }
 //等比数列求和
 //留意a=0的情况
-LL dbsum(LL a,LL b,LL mod){
-	if(b==0)
-		return 1;
-	if(b&1)
-		return (a_b_MOD_c(a,(b+1)/2,MOD)+1)*dbsum(a,b/2,MOD)%MOD;
-	else
-		return ((a_b_MOD_c(a,b/2,MOD)+1)*dbsum(a,(b-1)/2,MOD)%MOD + a_b_MOD_c(a,b,MOD))%MOD;
-} 
+LL powsummod(LL a, LL b, LL mod) {
+  if (b == 0)
+    return 1;
+  if (b & 1)
+    return (powmod(a, (b + 1) / 2, MOD) + 1) * powsummod(a, b / 2, MOD) % MOD;
+  else
+    return ((powmod(a, b / 2, MOD) + 1) * powsummod(a, (b - 1) / 2, MOD) % MOD +
+            powmod(a, b, MOD)) %
+           MOD;
+}
